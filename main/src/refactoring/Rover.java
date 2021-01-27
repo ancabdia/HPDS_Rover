33cbc675adecff1a17907879601005fecaba3f67
ancabdia <ancabdia.contact@gmail.com>
2021-01-27 14:14:28 +0000

    Rover refactor

--- main/src/refactoring/Rover.java
+++ main/src/refactoring/Rover.java
@@ -1,11 +1,73 @@
 package refactoring;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static refactoring.Rover.Heading.*;
+import static refactoring.Rover.Order.*;
+
 public class Rover {
 
+	private Heading heading;
+	private Position position;
+
 	public Rover(String facing, int x, int y) {
+		this(Heading.of(facing), x, y);
 	}
 
 	public Rover(Heading heading, int x, int y) {
+		this.heading = heading;
+		this.position = new Position(x,y);
+	}
+
+	public Rover(Heading heading, Position position) {
+		this.heading = heading;
+		this.position = position;
+	}
+
+	public Heading heading() {
+		return heading;
+	}
+
+	public Position position() {
+		return position;
+	}
+
+	public void go(Order... orders) {
+		execute(orders[0]);
+	}
+
+	private void execute(Order order) {
+		if (order == Left) heading = heading.turnLeft();
+		if (order == Right) heading = heading.turnRight();
+		if (order == Forward) position = position.forward(heading);
+		if (order == Backward) position = position.backward(heading);
+	}
+
+	private Map<Order, Action> actions = new HashMap<>();
+	{
+		actions.put(Left, () -> heading = heading.turnLeft());
+		actions.put(Right, () -> heading = heading.turnRight());
+		actions.put(Forward, () -> position = position.forward(heading));
+		actions.put(Backward, () -> position = position.backward(heading));
+	}
+
+	public void go(String orders) {
+		go(ordersIn(orders));
+	}
+
+	private Order[] ordersIn(String instructions) {
+		Order[] orders = new Order[instructions.length()];
+		for (int i = 0; i < instructions.length(); i++) {
+			orders[i] = Order.of(instructions.charAt(i));
+		}
+		return orders;
+	}
+
+
+	@FunctionalInterface
+	public interface Action {
+		void execute();
 	}
 
 	public static class Position {
@@ -18,7 +80,27 @@ public class Rover {
 		}
 
 		public Position forward(Heading heading) {
-			return null;
+			return new Position(x+dx(heading), y+dy(heading));
+		}
+
+		public Position backward(Heading heading) {
+			return new Position(x-dx(heading), y-dy(heading));
+		}
+
+		private int dx(Heading heading) {
+			return heading == East ? 1 : heading == West ? -1 : 0;
+		}
+
+		private int dy(Heading heading) {
+			return heading == North ? 1 : heading == South ? -1 : 0;
+		}
+
+		@Override
+		public String toString() {
+			return "Position{" +
+					"x=" + x +
+					", y=" + y +
+					'}';
 		}
 
 		@Override
@@ -34,6 +116,18 @@ public class Rover {
 			return object != null && object.getClass() == Position.class;
 		}
 
+	}
+
+	public enum Order {
+		Forward, Backward, Left, Right;
+
+		static Order of(char c) {
+			if (c == 'L') return Left;
+			if (c == 'R') return Right;
+			if (c == 'F') return Forward;
+			if (c == 'B') return Backward;
+			return null;
+		}
 	}
 
 
